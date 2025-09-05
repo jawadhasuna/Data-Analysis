e04fd9ac753627ee80def6e509aafdaed3a06842
jawadhasuna <171467245+jawadhasuna@users.noreply.github.com>
2025-09-05 18:30:41 +0500

    Create for.py

---
+++ for.py
@@ -0,0 +1,18 @@
+days =["monday","tuesday","wednesday","thursday","friday",]
+for day in days:
+  print(day)
+
+for char in "monday":
+  print(char)
+weeks=("one","two","three")
+for week in weeks:
+  print(week)
+person={
+    'name':"jawad",
+    'height' : 5.2,
+    'id' : 21
+    }
+for key in person:
+  print(person[key])
+for value in person.values():
+  print(value)
