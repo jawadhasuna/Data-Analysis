6851de15ff3f3c38dd124061b03352b13e21affa
jawadhasuna <171467245+jawadhasuna@users.noreply.github.com>
2025-09-14 20:39:20 +0500

    Create pandas.py

---
+++ pandas.py
@@ -0,0 +1,19 @@
+import os
+os.getcwd()
+os.makedirs("./data",exist_ok=True)
+os.chdir("/content/data")
+
+with open("sample_data.csv", "w") as f:
+    f.write("ID,Name,Age,Department,Salary\n")
+    f.write("1,Ali,25,IT,50000\n")
+    f.write("2,Ayesha,28,HR,55000\n")
+    f.write("3,Ahmed,30,Finance,60000\n")
+    f.write("4,Sara,26,Marketing,52000\n")
+    f.write("5,Hassan,32,IT,65000\n")
+import pandas as pd
+
+# read the CSV into a DataFrame
+df = pd.read_csv("sample_data.csv")
+
+# display it
+print(df)
